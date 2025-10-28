2aee0c9d1258e5f5a75a3d464e7e4d80b4ff3586
marbeiro <mbeiro@fi.uba.ar>
2025-10-27 22:46:33 -0300

    docker spark

--- Procesamiento_Distribuido/01_corregir_archivo.py
+++ Procesamiento_Distribuido/01_corregir_archivo.py
@@ -1,27 +1,18 @@
-from pyspark import SparkContext, SparkConf
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+from pyarrow import fs
+from pyarrow import dataset
+import re
 
-#################################################
-# Modo 1: Usamos la conexión en modo client (con el SparkContext en nuestra máquina)
+# Leemos el .csv a un DataFrame de Pandas
+df = pd.read_csv('/tmp/badata_ecobici_recorridos_realizados_2024.csv', names=['id_recorrido', 'duracion_recorrido', 'fecha_origen_recorrido',
+'id_estacion_origen', 'nombre_estacion_origen', 'direccion_estacion_origen', 
+'long_estacion_origen', 'lat_estacion_origen', 'fecha_destino_recorrido', 'id_estacion_destino', 'nombre_estacion_destino', 'direccion_estacion_destino', 'long_estacion_destino', 'lat_estacion_destino', 'id_usuario', 'modelo_bicicleta', 'genero'], header=1)
 
-# Creamos un contexto de Spark
-sc = SparkContext('local', 'test')
+# Eliminando '.' en duracion_recorrido para que Pandas pueda guardarlo como float.
+df['duracion_recorrido'] = [ re.sub('.0^', '', str(x)) for x in df['duracion_recorrido'] ]
+df['duracion_recorrido'] = df['duracion_recorrido'].str.replace('.', '')
+df.astype({'duracion_recorrido': 'float'})
 
-# Nos conectamos al cluster HDFS
-archivo = sc.textFile("hdfs://namenode:8020/recorridos_realizados_2024_corregido.csv")
-#################################################
-
-
-#################################################
-# Modo 2 (alternativo): Usar el SparkContext en YARN. Requiere tener python3+pyspark en todos los nodos de HDFS, cosa que no tenemos
-#
-# Creamos un contexto de Spark
-# sc = SparkContext(master='yarn')
-#
-# Nos conectamos al cluster HDFS
-# archivo = sc.textFile("/recorridos_realizados_2024.csv")
-#################################################
-
-
-# Ejecutamos la tarea, en este caso contar la cantidad de líneas del archivo
-archivo.count()
-
+df.to_csv('/tmp/recorridos_realizados_2024_corregido.csv', sep=';', header=False, index=False)
